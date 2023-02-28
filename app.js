a035f6dd13f3179d3a6ba8f49be04c75f81669e5
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-02-28 15:48:40 +0530

    class

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-var stud = {
-    name: "test",
-    age: 30,
-    phone: 12345678902
-};
-var studentList = [
-    { name: "test", age: 30 },
-    { name: "test2", age: 20 }
-];
-for (var i = 0; i < studentList.length; i++) {
-    var elem = studentList[i];
-    console.log("e", elem);
-}
-studentList.push(stud);
-console.log(studentList);
+// let stud  ={
+// name: "test",
+// age: 30,
+// phone: 12345678902
+// }
+// let studentList = [
+//     { name: "test",age: 30},
+//     { name: "test2",age: 20}
+// ]
+// for(let i=0;i<studentList.length;i++){
+//     let elem = studentList[i]
+//     console.log("e",elem)
+// }
+// studentList.push(stud)
+// console.log(studentList)
+// enum LanguageName {a,b,c}
+//interface
+// interface StudType{
+//     name:string,
+//     age:number,
+// }
+// let stud : StudType ={
+//     name: "test",
+//     age: 30,
+//     }
+//     let studentList : StudType[] = [
+//             { name: "test",age: 30},
+//             { name: "test2",age: 20}
+//         ]
+// const getData = (studentList :StudType[])=>{
+//     for(let i=0;i<studentList.length;i++){
+//             let elem = studentList[i]
+//             console.log("e",elem)
+//         }
+// }
+// getData(studentList)
+var Students = /** @class */ (function () {
+    function Students(name) {
+        this._name = name;
+    }
+    Students.prototype.getData = function () {
+        return this._name;
+    };
+    return Students;
+}());
+var s = new Students("chetan");
+console.log(s.getData());
--- app.ts
+++ app.ts
@@ -19,25 +19,43 @@
 
 // enum LanguageName {a,b,c}
 
+
 //interface
-interface StudType{
-    name:string,
-    age:number,
-}
+// interface StudType{
+//     name:string,
+//     age:number,
+// }
 
-let stud : StudType ={
-    name: "test",
-    age: 30,
+// let stud : StudType ={
+//     name: "test",
+//     age: 30,
+//     }
+//     let studentList : StudType[] = [
+//             { name: "test",age: 30},
+//             { name: "test2",age: 20}
+//         ]
+// const getData = (studentList :StudType[])=>{
+//     for(let i=0;i<studentList.length;i++){
+//             let elem = studentList[i]
+//             console.log("e",elem)
+//         }
+// }
+
+// getData(studentList)
+
+
+class Students {
+    private _name:string;
+
+    constructor(name:string){
+        this._name = name;
     }
-    let studentList : StudType[] = [
-            { name: "test",age: 30},
-            { name: "test2",age: 20}
-        ]
-const getData = (studentList :StudType[])=>{
-    for(let i=0;i<studentList.length;i++){
-            let elem = studentList[i]
-            console.log("e",elem)
-        }
+
+    getData(): string
+    {
+        return this._name
+    }
 }
 
-getData(studentList)
+let s = new Students("chetan")
+console.log(s.getData())
