23a81d1645b45f704f65975a702608aed1c38d0c
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-02-28 15:29:28 +0530

    interface

--- app.ts
+++ app.ts
@@ -1,18 +1,43 @@
-let stud ={
-name: "test",
-age: 30,
-phone: 12345678902
+// let stud  ={
+// name: "test",
+// age: 30,
+// phone: 12345678902
+// }
+
+// let studentList = [
+//     { name: "test",age: 30},
+//     { name: "test2",age: 20}
+// ]
+
+// for(let i=0;i<studentList.length;i++){
+//     let elem = studentList[i]
+//     console.log("e",elem)
+// }
+
+// studentList.push(stud)
+// console.log(studentList)
+
+// enum LanguageName {a,b,c}
+
+//interface
+interface StudType{
+    name:string,
+    age:number,
 }
 
-let studentList = [
-    { name: "test",age: 30},
-    { name: "test2",age: 20}
-]
-
-for(let i=0;i<studentList.length;i++){
-    let elem = studentList[i]
-    console.log("e",elem)
+let stud : StudType ={
+    name: "test",
+    age: 30,
+    }
+    let studentList : StudType[] = [
+            { name: "test",age: 30},
+            { name: "test2",age: 20}
+        ]
+const getData = (studentList :StudType[])=>{
+    for(let i=0;i<studentList.length;i++){
+            let elem = studentList[i]
+            console.log("e",elem)
+        }
 }
 
-studentList.push(stud)
-console.log(studentList)
+getData(studentList)
