0d6c38143461345e0062b91067dcc20751dffc00
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-03 13:33:58 +0530

    tuples

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -13,39 +12,25 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
-// GENERICS
-// const addUID = (obj: object) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-// const addUID = <T extends object>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docOne = addUID({ name: 'yoshi', age: 40 });
-//let docTwo = addUID('shaun');
-console.log(docOne.name);
-const docThree = {
-    uid: 1,
-    resourceName: 'person',
-    data: { name: 'shaun' }
-};
-const docFour = {
-    uid: 1,
-    resourceName: 'shoppingList',
-    data: ['bread', 'milk']
-};
-console.log(docThree, docFour);
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+let student;
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { ListTemplate } from './classes/ListTemplate.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
@@ -19,55 +18,29 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
   if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
   
   list.render(doc, type.value, 'end');
 });
 
-// GENERICS
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-// const addUID = (obj: object) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
 
-// const addUID = <T extends object>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-
-const addUID = <T extends {name: string}>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return {...obj, uid};
-}
-
-let docOne = addUID({name: 'yoshi', age: 40});
-//let docTwo = addUID('shaun');
-
-console.log(docOne.name);
-
-// with interfaces
-interface Resource<T> {
-  uid: number;
-  resourceName: string;
-  data: T;
-}
-
-const docThree: Resource<object> = {
-  uid: 1, 
-  resourceName: 'person', 
-  data: { name: 'shaun' }
-};
-
-const docFour: Resource<string[]> = {
-  uid: 1, 
-  resourceName: 'shoppingList', 
-  data: ['bread', 'milk']
-};
-
-console.log(docThree, docFour);
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
